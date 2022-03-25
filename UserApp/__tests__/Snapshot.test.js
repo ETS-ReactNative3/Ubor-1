7368f3e989c79d07f73eba69d6960d9944aac7c5
Aninda <anindaa.shome@gmail.com>
2022-03-25 11:01:23 -0600

    Snapshot tests done for User UI Interfaces

--- UserApp/__tests__/Snapshot.test.js
+++ UserApp/__tests__/Snapshot.test.js
@@ -1,10 +1,51 @@
 import CovidMessage from '../src/components/CovidMessage/index';
+import HomeMap from '../src/components/HomeMap';
+import HomeSearch from '../src/components/HomeSearch';
+import RouteMap from '../src/components/RouteMap';
+import UberTypeRow from '../src/components/UberTypeRow';
+import UberTypes from '../src/components/UberTypes';
+import CustomDrawer  from '../src/navigation/CustomDrawer';
+import HomeNavigator from '../src/navigation/Home';
+
 import React from 'react';
 import {create} from 'react-test-renderer';
 
 test('Covid Message Snapshot', () => {
     const tree = create(<CovidMessage />);
-    expect(tree).toMatchSnapshot;
+    expect(tree).toMatchSnapshot();
 });
 
+test('HomeMap Snapshot', () => {
+    const treeTwo = create(<HomeMap />);
+    expect(treeTwo).toMatchSnapshot();
+});
 
+test('HomeSearch Snapshot', () => {
+    const treeThree = create(<HomeSearch />);
+    expect(treeThree).toMatchSnapshot();
+});
+
+test('RouteMap Snapshot', () => {
+    const treeFour = create(<RouteMap />);
+    expect(treeFour).toMatchSnapshot();
+});
+
+test('UberTypeRow Snapshot', () => {
+    const treeFive = create(<UberTypeRow />);
+    expect(treeFive).toMatchSnapshot();
+});
+
+test('HomeNavigator Snapshot', () => {
+    const treeSix = create(<HomeNavigator />);
+    expect(treeSix).toMatchSnapshot();
+});
+
+test('CustomDrawer Snapshot', () => {
+    const treeSeven = create(<CustomDrawer />);
+    expect(treeSeven).toMatchSnapshot();
+});
+
+test('UberTypes Snapshot', () => {
+    const treeEight = create(<UberTypes />);
+    expect(treeEight).toMatchSnapshot();
+});
