30ba83d270f329d6200126fdde37b157cfe1f673
islamrahat735 <islamrahat735@gmail.com>
2022-03-24 23:06:42 -0600

    auth stuff

--- UserApp/App.js
+++ UserApp/App.js
@@ -19,9 +19,9 @@ import UberTypes from './src/components/UberTypes';
 
 navigator.geolocation = require('@react-native-community/geolocation');
 
-import Amplify from 'aws-amplify'
-import awsconfig from './aws-exports'
-import { withAuthenticator } from 'aws-amplify-react-native';
+import {Amplify, Auth} from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+import { withAuthenticator} from 'aws-amplify-react-native';
 Amplify.configure(awsconfig)
 
  
@@ -51,6 +51,9 @@ Amplify.configure(awsconfig)
   }
 
   useEffect(() => {
+    Auth.currentUserInfo().then(info => {console.log(info)});
+    //console.log(info)
+    console.log("HELOO")
     if (Platform.OS === 'android') {
       androidPermission();
     } else {
@@ -66,6 +69,48 @@ Amplify.configure(awsconfig)
      </>
    );
  };
+
+ const signUpConfig = {
+  header: "My Customized Sign Up",
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: "Username",
+      key: "username",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 2,
+      type: "string",
+    },
+    {
+      label: "First Name",
+      key: "custom:fname",
+      required: false,
+      displayOrder: 3,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 4,
+      type: "password",
+    },
+    {
+      label: "Phone Number",
+      key: "phone_number",
+      required: true,
+      displayOrder: 4,
+      type: "string",
+    }
+  ],
+};
  
- export default withAuthenticator(App);
+ export default withAuthenticator(App, {signUpConfig}) ;
  
---
+++ UserApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "whatisgoingonPostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "auth": {
+        "ibrahimauth": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "backendubor": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
