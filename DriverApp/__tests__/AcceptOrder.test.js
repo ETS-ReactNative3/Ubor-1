7d3791805299dc925f76c18ecc2d2ca285049990
ibrahim29asad <72579268+ibrahim29asad@users.noreply.github.com>
2022-03-28 23:44:24 -0600

    Update AcceptOrder.test.js

--- DriverApp/__tests__/AcceptOrder.test.js
+++ DriverApp/__tests__/AcceptOrder.test.js
@@ -3,6 +3,20 @@ import React from 'react';
 // import { submitDest } from '../screens/DestinationPage/DestinationPage'
 import AcceptOrder from '../src/screens/HomeScreen/AccpetOrderAndDestination'
 
+/**
+ * Testing if an Order is accepted from the DriverSide
+ * @param {String} temp Name of Location
+ * @param {Float} order.id ID of the Order
+ * @param {String} order.type Type of Service
+ * @param {Float} order.originLatitude The Latitude of the Origin
+ * @param {Float} order.oreiginLongitude The Longitude of the Origin
+ * @param {Float} order.destLatitude The Latitude of the Destination
+ * @param {Float} order.destLongitude The Longitude of the Destination
+ * @param {Boolean} order.accept If the Driver accpets the Riders Request
+ * 
+ */
+
+
 test('accepting a order', () => {
   var temp = "University of Calgary";
   var order = {
@@ -17,6 +31,18 @@ test('accepting a order', () => {
   expect(AcceptOrder(order)).toBe("accepted order");
 });
 
+/**
+ * Testing if an Order is declined from the DriverSide
+ * @param {String} temp Name of Location
+ * @param {Float} order.id ID of the Order
+ * @param {String} order.type Type of Service
+ * @param {Float} order.originLatitude The Latitude of the Origin
+ * @param {Float} order.oreiginLongitude The Longitude of the Origin
+ * @param {Float} order.destLatitude The Latitude of the Destination
+ * @param {Float} order.destLongitude The Longitude of the Destination
+ * @param {Boolean} order.accept If the Driver accpets the Riders Request
+ * 
+ */
 
 test('declining a order', () => {
     var temp = "University of Calgary";
